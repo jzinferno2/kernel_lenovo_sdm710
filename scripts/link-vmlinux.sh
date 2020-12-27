6b58ff4d45a067182681863ee2f70949e34626ad
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-26 19:58:22 -0800

    lto: Add Link Time Optimization support for GCC
    This requires a modern version of GCC and various other patches in order
    to work. LTO results in a smaller kernel binary with better performance.
    
    Based off of work from Andi Kleen <ak@linux.intel.com>.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -108,7 +108,7 @@ modpost_link()
 		info LD vmlinux.o
 	fi
 
-	${LD} ${KBUILD_LDFLAGS} -r -o ${1} $(modversions) ${objects}
+	${LDFINAL} ${KBUILD_LDFLAGS} -r -o ${1} $(modversions) ${objects}
 }
 
 # If CONFIG_LTO_CLANG is selected, we postpone running recordmcount until
@@ -133,7 +133,7 @@ vmlinux_link()
 	local objects
 
 	if [ "${SRCARCH}" != "um" ]; then
-		local ld=${LD}
+		local ld=${LDFINAL}
 		local ldflags="${KBUILD_LDFLAGS} ${LDFLAGS_vmlinux}"
 
 		if [ -n "${LDFINAL_vmlinux}" ]; then
